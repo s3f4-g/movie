a79a6c74c924bb03425df01427bca663d9621c2c
sefa can  genc <sefacan19.sg@gmail.com>
2023-03-13 15:19:20 +0300

    edited card section

---
+++ src/_codux/boards/card/card.board.tsx
@@ -0,0 +1,21 @@
+import { createBoard } from '@wixc3/react-board';
+import { Card } from '../../../components/card/card';
+
+export default createBoard({
+    name: 'Card',
+    Board: () => (
+        <Card
+            movie={{
+                id: '123',
+                overview:
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+                release_date: '11.11.2023',
+                vote_avarage: '9.2',
+                poster_path:
+                    'https://posters.movieposterdb.com/20_10/2019/7286456/l_7286456_97fee279.jpg',
+                title: 'Joker',
+            }}
+            className={''}
+        />
+    ),
+});
