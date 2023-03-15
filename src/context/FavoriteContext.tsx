ad5773c8b4eb8f6a6c03add2a39dee715373ef54
sefa can  genc <sefacan19.sg@gmail.com>
2023-03-16 00:54:55 +0300

    hooks

---
+++ src/context/FavoriteContext.tsx
@@ -0,0 +1,60 @@
+import React, { createContext, useReducer } from 'react';
+
+interface Movie {
+  id: number;
+  poster_path?: string;
+  title: string;
+  overview: string;
+  vote_average: number;
+  release_date: string;
+}
+
+interface State {
+  favorites: Movie[];
+}
+
+interface FavoriteAction {
+  type: string;
+  payload: any;
+}
+
+const initialState: State = {
+  favorites: [],
+};
+
+
+
+const FavoriteReducer = (state: State, action: FavoriteAction) => {
+  switch (action.type) {
+    case 'ADD_MOVIE':
+      return { ...state, favorites: [...state.favorites, action.payload] };
+    case 'REMOVE_MOVIE':
+      return { favorites: state.favorites.filter(item => item.id !== action.payload.id),
+      };
+    default:
+      return state;
+  }
+};
+
+
+interface ProviderProps{
+    children:React.ReactNode
+}
+
+export const FavoriteContext = createContext<{
+  state: State;
+  dispatch: React.Dispatch<FavoriteAction>;
+}>({
+  state: initialState,
+  dispatch: () => {}});
+
+export const FavoriteContextProvider: = ({ children }:ProviderProps) => {
+  const [state, dispatch] = useReducer(FavoriteReducer, initialState);
+
+  return (
+    <FavoriteContext.Provider value={{ state, dispatch }}>
+      {children}
+    </FavoriteContext.Provider>
+  );
+};
+
